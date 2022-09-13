c85de1619e538840e5e8ede740106c264a9fe106
Kirill Gureev <kir.gureev2010@ya.ru>
2022-09-13 18:16:39 +0300

    Commit 3. Add file .appveyor.yml

--- src/test/java/ru/netology/TestingWebInterfaces.java
+++ src/test/java/ru/netology/TestingWebInterfaces.java
@@ -1,4 +1,9 @@
 package ru.netology;
 
 public class TestingWebInterfaces {
+    // ChromeOptions options = new ChromeOptions();
+    //options.addArguments("--disable-dev-shm-usage");
+    //options.addArguments("--no-sandbox");
+    //options.addArguments("--headless");
+    //driver = new ChromeDriver(options);
 }
